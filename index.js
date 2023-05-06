a0a33e3d65f250f2f1f9c5345dc77bd181d8ffc1
kuldeepsaini23 <kuldeepsaini02311@gmail.com>
2023-05-06 17:33:15 +0530

    Function to EventListener on contac Button

--- index.js
+++ index.js
@@ -1,46 +1,40 @@
-const firstName = document.getElementById("inputFirstName").value;
-const lastName = document.getElementById("inputLastName").value;
-const email = document.getElementById("inputEmail").value;
-const message = document.getElementById("message").value;
-
 const button = document.getElementById("btn-contact");
 
-// button.addEventListener("click", ()=>{
-//   console.log(firstName)
-//   console.log(lastName)
-//   console.log(email)
-//   console.log(message)
-// })
+button.addEventListener("click", async() => {
+  const firstName = document.getElementById("inputFirstName").value;
+  const lastName = document.getElementById("inputLastName").value;
+  const email = document.getElementById("inputEmail").value;
+  const message = document.getElementById("message").value;
+  console.log("FirstName: ", firstName);
+  console.log("LastName: ", lastName);
+  console.log("Email: ", email);
+  console.log("Message: ", message);
 
-console.log("helllloooooo");
-
-async function sendEmail() {
-  console.log("helllloooooo2");
+  if (firstName.length > 0 || lastName.length > 0 || email.length > 0 || message.length > 0) {
     try {
       const response = await fetch(
-        `https://www.grupverse.com/companywebsitemsg`,
-        {
+        `https://www.grupverse.com/companywebsitemsg`, {
           method: "POST",
           body: JSON.stringify({
             name: `${firstName} ${lastName}`,
             email: email,
             msg: message,
             completed: false,
-           })
+          })
           //,
           //   headers: {
           //     "Content-Type": "application/x-www-form-urlencoded",
           //   },
         }
       );
-  
+
       const data = await response.json();
       Swal.fire(
         'Successfull!',
         'You email is been Recieved',
         'success'
       )
-        
+
     } catch (err) {
       console.log(err);
       Swal.fire(
@@ -49,4 +43,5 @@ async function sendEmail() {
         'error'
       )
     }
-}
+  }
+});
